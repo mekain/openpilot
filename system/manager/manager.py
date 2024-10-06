2a4456cc6fbc74f073ed835048955a5be15b22c0
1okko <136616964+1okko@users.noreply.github.com>
2024-10-06 11:50:53 +0800

    Update manager.py

--- system/manager/manager.py
+++ system/manager/manager.py
@@ -38,6 +38,7 @@ def manager_init() -> None:
     ("CompletedTrainingVersion", "0"),
     ("DisengageOnAccelerator", "0"),
     ("GsmMetered", "1"),
+    ("IsMetric", "1"),
     ("HasAcceptedTerms", "0"),
     ("LanguageSetting", "main_zh-CHS"),
     ("OpenpilotEnabledToggle", "1"),
