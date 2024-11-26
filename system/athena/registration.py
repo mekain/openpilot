37deb6198bbe31d36518ee68b6ee5706cbd704ab
1okko <136616964+1okko@users.noreply.github.com>
2024-11-26 11:33:36 +0800

    Update registration.py

--- system/athena/registration.py
+++ system/athena/registration.py
@@ -46,21 +46,10 @@ def register(show_spinner=False) -> str | None:
     # Block until we get the imei
     serial = HARDWARE.get_serial()
     start_time = time.monotonic()
-    imei1: str | None = None
-    imei2: str | None = None
-    while imei1 is None and imei2 is None:
-      try:
-        imei1, imei2 = HARDWARE.get_imei(0), HARDWARE.get_imei(1)
-      except Exception:
-        cloudlog.exception("Error getting imei, trying again...")
-        time.sleep(1)
-
-      if time.monotonic() - start_time > 60 and show_spinner:
-        spinner.update(f"registering device - serial: {serial}, IMEI: ({imei1}, {imei2})")
-
+    imei1='865420071781912'
+    imei1='865420071781904'
     params.put("IMEI", imei1)
     params.put("HardwareSerial", serial)
-
     backoff = 0
     start_time = time.monotonic()
     while True:
@@ -83,7 +72,7 @@ def register(show_spinner=False) -> str | None:
         time.sleep(backoff)
 
       if time.monotonic() - start_time > 60 and show_spinner:
-        spinner.update(f"registering device - serial: {serial}, IMEI: ({imei1}, {imei2})")
+        return UNREGISTERED_DONGLE_ID
 
     if show_spinner:
       spinner.close()
