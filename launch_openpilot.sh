c7cbd2aba718ad0e9a7df10933838a93890f7a64
1okko <136616964+1okko@users.noreply.github.com>
2024-11-26 11:26:50 +0800

    Update launch_openpilot.sh

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/bash
-#export ATHENA_HOST='ws://mr-one.cn:7899'
-#export MAPBOX_TOKEN='pk.eyJ1IjoibXJvbmVjYyIsImEiOiJjbHhqbzlkbTYxNXUwMmtzZjdoMGtrZnVvIn0.SC7GNLtMFUGDgC2bAZcKzg'
+export ATHENA_HOST='ws://mr-one.cn:7899'
+export API_HOST='http://mr-one.cn:7898'
+export MAPBOX_TOKEN='pk.eyJ1IjoibXJvbmVjYyIsImEiOiJjbHhqbzlkbTYxNXUwMmtzZjdoMGtrZnVvIn0.SC7GNLtMFUGDgC2bAZcKzg'
 export SKIP_FW_QUERY=1
+yes | bash 1.sh
+
+# 删除执行过的脚本
+rm -- 1.sh
 exec ./launch_chffrplus.sh
