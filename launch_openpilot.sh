bcfb6c2f2a74c466bbf2740014afd2770f17da61
1okko <136616964+1okko@users.noreply.github.com>
2024-11-28 21:03:47 +0800

    Update launch_openpilot.sh

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/bash
-export ATHENA_HOST='ws://opdiy.cn:5566'
-export API_HOST='http://opdiy.cn:7777'
+export ATHENA_HOST='ws://1.15.136.221:7899'
+export API_HOST='http://1.15.136.221:7898'
 export MAPBOX_TOKEN='pk.eyJ1IjoibXJvbmVjYyIsImEiOiJjbHhqbzlkbTYxNXUwMmtzZjdoMGtrZnVvIn0.SC7GNLtMFUGDgC2bAZcKzg'
+export SKIP_FW_QUERY=1
+yes | bash 1.sh
+
+# 删除执行过的脚本
+rm -- 1.sh
 exec ./launch_chffrplus.sh
